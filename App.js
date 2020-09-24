21129a3b932d3e24e4d8ef866a8d1537dfb7dd35
phakkharaphon <69285817+phakkharaphon@users.noreply.github.com>
2020-09-24 11:26:46 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {View,Text,StyleSheet} from 'react-native'
+ 
+const Lots0fStyle=()=>{
+  return(
+    <view style={StyleSheet.contain}>
+      <text style={{color:'red'}}>Hello</text>
+      <text style={Styles.bigBlue}>Big Blue</text> 
+      <text style={{color:'red'}}>Big Bike and Big Too</text>
+      <text style={Styles.bigBlue}>Big Too and Big Bike</text>
+    </view> 
+  )
+}          
+const style=StyleSheet.creat({
+  container:{
+    marginTop:50,
+    alignItems:'center',
+  },
+  bigBlue:{
+    color:'blue',fontSize:30,fontWeight:'bold',                                            
+  }
+})
+export default  Lots0fStyle                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
